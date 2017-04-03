ccad8b664cdd6bf737a864859fe492d1cf6d3f0b
Aline Lee Nogueira and Tiago Celestino <login+alinelee+tcelestino@elo7.com>
2017-04-03 15:08:24 -0300

    Esconde o avatar quando o github não devolver a foto do usuário

--- src/assets/js/postAvatar.js
+++ src/assets/js/postAvatar.js
@@ -2,20 +2,26 @@ define(['doc', 'ajax'], function($, ajax) {
 
   var GITHUB_URI = 'https://api.github.com/users/',
       $author = $('p[itemprop="publisher"]'),
-      dataAuthor = $author.data('author'),
-      userInfo = {};
+      dataAuthor = $author.data('author');
 
 
   if($author) {
     ajax.get(GITHUB_URI + dataAuthor, {
       'X-RateLimit-Limit': 20
     }, {
-      success: function(response) {
-        userInfo = {
-          avatar: response.avatar_url + '&size=50',
-          name: response.name
-        };
-        $('.avatar').first().src = userInfo.avatar;
+      success: function(response, xhr) {
+        var $avatar = $('.avatar'),
+            userInfo = {
+              avatar: response.avatar_url + '&size=50',
+              name: response.name
+            };
+
+        if(xhr.status === 403) {
+          $avatar.addClass('hide');
+          return;
+        }
+
+        $avatar.first().src = userInfo.avatar;
         $author.html(userInfo.name);
       },
       error: function(response) {},
